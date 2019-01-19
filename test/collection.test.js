e1d50ba44e254622a5a21938e45d1832d928fe30
TK <leandrotk100@gmail.com>
2019-01-19 14:45:53 -0200

    Implement tests to collection module

--- test/collection.test.js
+++ test/collection.test.js
@@ -7,3 +7,19 @@ test('gets the first element from an array', () => {
 test('gets the second element from an array', () => {
   expect(second([1, 2])).toBe(2);
 });
+
+describe('with an empty array', () => {
+  test('gets undefined', () => {
+    expect(first([])).toBe(undefined);
+  });
+
+  test('gets undefined', () => {
+    expect(second([])).toBe(undefined);
+  });
+});
+
+describe('with only one element in the array', () => {
+  test('gets undefined', () => {
+    expect(second([1])).toBe(undefined);
+  })
+});
